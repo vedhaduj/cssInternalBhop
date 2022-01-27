db02b13b2597860e28653e8ab9047d1c8c7413f0
vedhaduj <97368023+vedhaduj@users.noreply.github.com>
2022-01-27 06:12:31 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <Windows.h>
+#include <iostream>
+#include <thread>
+
+int main()
+{
+	DWORD localplayer = 0x04C88E8;
+	DWORD flags = 0x350;
+	DWORD forcejump = 0x4F5D24;
+	DWORD gameModule;
+	gameModule = (DWORD)GetModuleHandle("client.dll");
+	localplayer = *(DWORD*)(gameModule + localplayer);
+
+	MessageBox(0, "Loading gamemodule + localplayer", "Loading gamemodule + localplayer", MB_OK);
+
+	if (localplayer == NULL) //sort of broken since starting a new game will cause the bhop not to work
+		while (localplayer == NULL)
+			  localplayer = *(DWORD*)(gameModule + localplayer);
+		
+	while (!GetAsyncKeyState(VK_END))
+	{
+		byte flag = *(BYTE*)(localplayer + flags);
+
+		if (GetAsyncKeyState(VK_SPACE) && flag & (1 << 0)) //do not switch! 
+		{			
+		  *(DWORD*)(gameModule + forcejump) = 6;		
+		}
+	}
+}
+
+//dll injecto!
+BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+	if (dwReason == DLL_PROCESS_ATTACH) {
+
+		DisableThreadLibraryCalls(hModule);
+		CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)main, NULL, NULL, NULL);
+		MessageBox(0, "Injected Succesfully", "Injected Succesfully", MB_OK);
+	
+	}
+	return TRUE;
+}
